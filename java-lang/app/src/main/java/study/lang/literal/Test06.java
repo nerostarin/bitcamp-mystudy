c8e6bf4a6d37c608a36cca124ba753b228959a84
LeeTaeJung <nerostarin@naver.com>
2024-06-03 19:03:08 +0900

    문자조합/메모리에데이터저장

---
+++ java-lang/app/src/main/java/study/lang/literal/Test06.java
@@ -0,0 +1,12 @@
+package study.lang.literal;
+// 실습
+
+// - 12.375f 부동소수점을 출력하라
+// - 컴파일된 클래스 파일을 16진수 코드로 읽어서 12.375 저장된것을 확인하라
+
+//-제어문자(escape character)의 활용 연습 
+public class Test06 {
+    public static void main(String[] args) {
+        System.out.println(12.375f);// 부동소수점이 메모리에 저장될 때 2진수로 바뀌는것을 확인하라
+    }
+}
