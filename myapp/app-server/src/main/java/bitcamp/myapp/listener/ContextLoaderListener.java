b00c4f699ece7f3978577f0ccb08ed9d6ae57707
LeeTaeJung <nerostarin@naver.com>
2024-09-23 18:26:53 +0900

    0923-2

--- myapp/app-server/src/main/java/bitcamp/myapp/listener/ContextLoaderListener.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/listener/ContextLoaderListener.java
@@ -2,12 +2,14 @@ package bitcamp.myapp.listener;
 
 import bitcamp.myapp.config.AppConfig;
 import bitcamp.myapp.context.ApplicationContext;
+import bitcamp.myapp.filter.CharacterEncodingFilter;
+import bitcamp.myapp.servlet.DispatcherServlet;
 import org.apache.ibatis.session.SqlSessionFactory;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import javax.servlet.*;
 import javax.servlet.annotation.WebListener;
+import java.io.File;
+import java.util.EnumSet;
 
 @WebListener // 서블릿 컨테이너에 이 클래스를 배치하는 태그다.
 public class ContextLoaderListener implements ServletContextListener {
@@ -22,8 +24,34 @@ public class ContextLoaderListener implements ServletContextListener {
             ApplicationContext iocContainer = new ApplicationContext(ctx, AppConfig.class);
 
             ctx.setAttribute("sqlSessionFactory", iocContainer.getBean(SqlSessionFactory.class));
-            ctx.setAttribute("controllers", iocContainer.getControllers());
 
+            //프론트 컨트롤러 역항르 수행 할 서블릿 객체 생성
+            DispatcherServlet dispatcherServlet = new DispatcherServlet(iocContainer);
+
+            //서블릿 컨테이너에 서블릿을 등록하기
+            ServletRegistration.Dynamic servletRegistration = ctx.addServlet("app", dispatcherServlet);
+
+            // 서블릿 정보를 설정하는 객체를 이용하여 서블릿에 URL 연결
+            servletRegistration.addMapping("/app/*");//URL 매핑
+            servletRegistration.setLoadOnStartup(1);// 웹애플리케이션 시작 할 때 객체 자동 생성
+            servletRegistration.setMultipartConfig(new MultipartConfigElement(//멀티 파트 설정
+                    new File("./temp").getAbsolutePath(),//업로드 파일을 임시 보관할 폴더
+                    1024 * 1024 * 60,
+                    1024 * 1024 * 100,
+                    1024 * 1024 * 1));
+
+            //필터 객체 생성
+            CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter("UTF-8");
+
+            // 필터 객체를 서블릿 컨테이너에 등록
+            FilterRegistration.Dynamic filterRegistration = ctx.addFilter("characterEncodingFilter", characterEncodingFilter);
+
+            //필터 객체를 설정
+            filterRegistration.addMappingForServletNames(
+                    // 필터를 어떤 상황에서 동작하도록 할 것인지 지정
+                    EnumSet.of(DispatcherType.REQUEST, DispatcherType.INCLUDE, DispatcherType.FORWARD),
+                    false, //web.xml 에 설정된 매핑정보를 적용한 후에 필터 정보를 설정할 것인지
+                    "app");// 필터를 적용할 서블릿의 별명
 
         } catch (Exception e) {
             System.out.println("서비스 객체 준비 중 오류 발생!");
