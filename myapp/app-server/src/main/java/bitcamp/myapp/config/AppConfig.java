4e638ba1bb2a9593a7d002465dd975bab3f6d3f7
LeeTaeJung <nerostarin@naver.com>
2024-09-24 11:08:06 +0900

    0924

--- myapp/app-server/src/main/java/bitcamp/myapp/config/AppConfig.java
+++ myapp/app-server/src/main/java/bitcamp/myapp/config/AppConfig.java
@@ -5,7 +5,6 @@ import bitcamp.myapp.dao.BoardDao;
 import bitcamp.myapp.dao.DaoFactory;
 import bitcamp.myapp.dao.ProjectDao;
 import bitcamp.myapp.dao.UserDao;
-import bitcamp.myapp.service.*;
 import bitcamp.mybatis.SqlSessionFactoryProxy;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -22,7 +21,7 @@ public class AppConfig {
 
         return new SqlSessionFactoryProxy(sqlSessionFactory);
     }
-    
+
     @Bean
     public DaoFactory createDaoFactory(SqlSessionFactory sqlSessionFactory) throws Exception {
         return new DaoFactory(sqlSessionFactory);
@@ -41,20 +40,5 @@ public class AppConfig {
     @Bean
     public ProjectDao createProjectDao(DaoFactory daoFactory) throws Exception {
         return daoFactory.createObject(ProjectDao.class);
-    }
-
-    @Bean
-    public UserService createUserService(UserDao userDao, SqlSessionFactory sqlSessionFactory) throws Exception {
-        return new DefaultUserService(userDao, sqlSessionFactory);
-    }
-
-    @Bean
-    public BoardService createBoardService(BoardDao boardDao, SqlSessionFactory sqlSessionFactory) throws Exception {
-        return new DefaultBoardService(boardDao, sqlSessionFactory);
-    }
-
-    @Bean
-    public ProjectService createProjectService(ProjectDao projectDao, SqlSessionFactory sqlSessionFactory) throws Exception {
-        return new DefaultProjectService(projectDao, sqlSessionFactory);
     }
 }
